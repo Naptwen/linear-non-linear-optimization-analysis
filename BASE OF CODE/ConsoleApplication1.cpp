45e22ca48cba55410ef55d00d3733092c78de0a1
Naptwen <47798805+Naptwen@users.noreply.github.com>
2021-04-01 01:24:47 -0700

    Add files via upload

---
+++ BASE OF CODE/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include "embPython.h"
+using namespace std;
+int main()
+{
+
+	string rec = PySimpleRun("pyemb", "hello", "USG");
+
+	printf("Bow Wow");
+	_getch();
+	return 0;
+}
